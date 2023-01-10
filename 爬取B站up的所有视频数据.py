de4e0e055bea290050763cbf65232894c908e091
Zhao <100524789+U202142209@users.noreply.github.com>
2023-01-10 20:28:19 +0800

    Update 爬取B站up的所有视频数据.py

--- 爬取B站up的所有视频数据.py
+++ 爬取B站up的所有视频数据.py
@@ -47,8 +47,3 @@ if __name__ == '__main__':      # 主函数
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36 Edg/108.0.1462.76'
     }               # headers 反反爬虫
     getallvideos()          # 调用函数
-
-    # 98573631 鹿小草QAQ
-    # 291377718 四郎讲棋
-    # 39665558 偶尔有点小迷糊
-
